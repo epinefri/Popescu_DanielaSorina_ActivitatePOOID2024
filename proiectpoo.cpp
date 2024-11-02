991df13f75439120e5d528fad729bfc9fafdbc7c
epinefri <danielasorinap@gmail.com>
2024-11-02 20:45:24 +0200

    faza 1, pas 1

--- proiectpoo.cpp
+++ proiectpoo.cpp
@@ -1,10 +1,30 @@
-// proiectpoo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Alege?i trei obiecte din realitate.Cele trei obiecte trebuie s? fie din acela?i domeniu.Domeniul îl
+alege?i astfel încât s? con?in? ini?ialele voastre.Exemplu: Alin Zamfiroiu - Magazin, sau Ziar,
+etc. Pentru cele trei obiecte realiza?i trei clase care s? con?in? atribute specifice fiec?rui obiect în
+parte.
+Daniela Popescu - Dispozitive: telefon(model, capacitate baterie, sistem operare), tableta(dimensiune ecran, capacitate stocare, rezolutie), smartwatch(model, rezistenta la apa, gps) */ 
 
 #include <iostream>
 using namespace std;
-int main()
-{
-    cout << "test\n";
-}
 
+class telefon {
+	string model;
+	int capacitate_baterie;
+	string sistem_operare;
+};
+
+class tableta {
+	float dimensiune_ecran;
+	int capacitate_stocare;
+	int rezolutie;
+};
+
+class smartwatch {
+	string model;
+	bool rezistenta_apa;
+	bool gps;
+};
+
+int main(){
+
+}
